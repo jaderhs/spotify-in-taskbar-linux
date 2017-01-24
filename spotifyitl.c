066b7f0c2ae2c3d92f453e9299ec33646f47f677
Jader H. Silva <jaderhs5@gmail.com>
2017-01-24 17:13:15 -0200

    Add -w (wait) optional argument

--- spotifyitl.c
+++ spotifyitl.c
@@ -4,6 +4,8 @@
 #define WNCK_I_KNOW_THIS_IS_UNSTABLE 1
 #include <libwnck/libwnck.h>
 
+GMainLoop *main_loop = NULL;
+
 gint match_spotify_gdk_window(GdkWindow *window, gulong *xid) {
 
 	if( GDK_WINDOW_XID(window) == *xid )
@@ -37,20 +39,52 @@ void spotify_lookup_and_fix(WnckWindow *window, GList *gdk_window_list) {
 	gdk_window_set_type_hint(gdkWindow, GDK_WINDOW_TYPE_HINT_NORMAL);
 	gdk_window_show(gdkWindow);
 	gdk_flush();
+
+	if(main_loop != NULL) {
+		g_main_loop_quit(main_loop);
+		g_main_loop_unref(main_loop);
+		main_loop = NULL;
+	}
+}
+
+gboolean iter_windows(gpointer userdata) {
+
+	GList *gdk_window_stack;
+	WnckScreen *screen = wnck_screen_get_default();
+	GdkScreen *gdkScreen = gdk_screen_get_default();
+
+	gdk_window_stack = gdk_screen_get_window_stack(gdkScreen);
+
+	if(userdata != NULL)
+		wnck_screen_force_update(screen);
+
+	g_list_foreach(wnck_screen_get_windows(screen),
+				(GFunc)spotify_lookup_and_fix,
+						gdk_window_stack);
+
+	g_list_free_full(gdk_window_stack, g_object_unref);
+	return TRUE;
 }
 
 int main(int argc, char *argv[]) {
 
-	WnckScreen *screen;
-
 	gdk_init(&argc, &argv);
 
-	screen = wnck_screen_get_default();
-	wnck_screen_force_update(screen);
+	while(argc > 1) {
+		if(strcmp(argv[--argc], "-w") == 0 && main_loop == NULL) {
+			main_loop = g_main_loop_new(NULL, FALSE);
+		}
+	}
 
-	g_list_foreach(wnck_screen_get_windows(screen),
-					(GFunc)spotify_lookup_and_fix,
-					gdk_screen_get_window_stack(gdk_screen_get_default()));
+	iter_windows(&argc);
+	if(main_loop != NULL) {
+
+		g_timeout_add_seconds(2, iter_windows, NULL);
+		g_main_loop_run(main_loop);
+	} else {
+
+		iter_windows(&argc);
+	}
 
 	wnck_shutdown();
 
